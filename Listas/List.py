62b645623dcd62c10faa5465b4f7c0d1e4ac1338
WellyntonSouza <53615386+dMuto@users.noreply.github.com>
2022-05-18 22:04:04 -0300

    Estudo Lista

---
+++ Listas/List.py
@@ -0,0 +1,15 @@
+# INVENTARIO
+inventario = []
+resposta = "S"
+
+while resposta == "S":
+    inventario.append(input("Equipamento: "))
+    inventario.append(float(input("Valor: ")))
+    inventario.append(int(input("Número Serial: ")))
+    inventario.append(input("Departamento: "))
+    resposta = input("Digite \"S\" para continuar: ").upper() # O * \"S\" * é para imprimir as "" no console e
+                                                              # não para que seja interpretado no codigo
+
+
+for elemento in inventario:
+    print(elemento)
