b16fdc30fefa628af41bf80048d78f62418ed4a2
WellyntonSouza <53615386+dMuto@users.noreply.github.com>
2022-05-19 22:34:01 -0300

    inicialização de projeto MonstroDeDuelo
    Refazendo projeto criado na faculdade em Python

---
+++ Monstros De Duelo/Monstros.py
@@ -0,0 +1,82 @@
+"""from abc import ABC, abstractmethod
+
+class Monstros(ABC):
+
+    def monstros(self, nome: str, tipo: str, ataque: float, defesa: float):
+        self.nome = nome
+        self.tipo = tipo
+        self.ataque = ataque
+        self.defesa = defesa
+
+        pass
+
+    # estudar abstrações em python depois
+"""
+
+class Cavaleiro():
+
+    def __init__(self, nome: str, tipo: str, ataque: float, defesa: float):
+        self.nome = nome
+        self.tipo = tipo
+        self.ataque = ataque
+        self.defesa = defesa
+
+        self.ativar_efeito_cavaleiro()
+
+    def efeito_cavaleiro(self):
+        efeito = "Cavaleiros ganham 32% do seu ataque em defesa"
+
+        return efeito
+
+    def ativar_efeito_cavaleiro(self):
+
+        efeito = float(self.defesa) + (float(self.ataque * 0.32))
+        ganho_efeito = (float(self.ataque * 0.32))
+        self.defesa = efeito
+
+        return ganho_efeito
+
+
+class Dragao():
+
+    def __init__(self, nome: str, tipo: str, ataque: float, defesa: float):
+        self.nome = nome
+        self.tipo = tipo
+        self.ataque = ataque
+        self.defesa = defesa
+
+        self.ativar_efeito_dragao()
+
+    def efeito_dragao(self):
+        efeito = "Dragões ganham mais 15% do valor do seu ataque em ataque"
+
+        return efeito
+
+    def ativar_efeito_dragao(self):
+
+        efeito = float(self.ataque) + (float(self.ataque * 0.15))
+        ganho_efeito = (float(self.ataque * 0.15))
+
+        self.ataque = efeito
+
+        return ganho_efeito
+
+
+class Mago():
+
+    def __init__(self, nome: str, tipo: str, ataque: float, defesa: float):
+        self.nome = nome
+        self.tipo = tipo
+        self.ataque = ataque
+        self.defesa = defesa
+
+
+    def efeito_mago(self):
+
+        efeito = "Magos ganham 63% a mais de seu ataque, porem perdem 26% de defesa"
+
+        return efeito
+
+    def ativar_efeito_mago(self):
+
+        efeito = ""
