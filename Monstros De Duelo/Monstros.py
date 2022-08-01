941bba3228bee7a29f7fc7057a46fdbf8353b2dd
WellyntonSouza <53615386+dMuto@users.noreply.github.com>
2022-08-01 19:01:16 -0300

    adição de mago

--- Monstros De Duelo/Monstros.py
+++ Monstros De Duelo/Monstros.py
@@ -70,6 +70,8 @@ class Mago():
         self.ataque = ataque
         self.defesa = defesa
 
+        self.ativar_efeito_mago()
+
 
     def efeito_mago(self):
 
@@ -79,4 +81,27 @@ class Mago():
 
     def ativar_efeito_mago(self):
 
-        efeito = ""
+        efeito = float(self.ataque) + float(self.ataque * 0.63)
+        efeito_2 = float(self.defesa) - float(self.defesa * 0.26)
+        ganho_efeito = float(self.ataque * 0.63)
+        ganho_efeito_text = "Ganhou {value:.2f} de ataque"
+        self.ataque = efeito
+        self.defesa = efeito_2
+
+        return ganho_efeito_text.format(value = ganho_efeito)
+
+
+magos = []
+
+magoNegro = Mago(nome = 'Mago Negro', tipo = 'mago', ataque = 2500, defesa = 2200)
+magaNegra = Mago(nome = 'Maga Negra', tipo = 'mago', ataque = 2400, defesa = 2100)
+
+magos.append(magoNegro)
+magos.append(magaNegra)
+
+for range in magos:
+    print(range.nome)
+    print(range.tipo)
+    print(range.ataque)
+    print(range.defesa)
+    print(range.efeito_mago())
